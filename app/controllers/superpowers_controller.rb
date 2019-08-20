53d87147b0fb445080868788dd0ad5b6fb9be0ee
Grant Thomas <grantwilliamthomas@hotmail.co.uk>
2019-08-20 14:48:55 +0200

    finished edit and update methods in superpower

--- app/controllers/superpowers_controller.rb
+++ app/controllers/superpowers_controller.rb
@@ -15,12 +15,26 @@ class SuperpowersController < ApplicationController
   end
 
   def edit
+    set_superpower
   end
 
   def update
+    set_superpower
+    if @superpower.update(superpower_params)
+      redirect_to superpowers_path
+    else
+      render :edit
+    end
   end
 
   def destroy
   end
 
+  def set_superpower
+    @superpower = Superpower.find(params[:id])
+  end
+
+  def superpower_params
+    params.require(:superpower).permit(:name, :description, :photo)
+  end
 end
