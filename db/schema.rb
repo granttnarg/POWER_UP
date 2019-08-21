44bcca799222e4d267b616c2a2baaab42d3ffa35
Grant Thomas <grantwilliamthomas@hotmail.co.uk>
2019-08-21 13:07:38 +0200

    adding the availability column and setting the default to true for superpower model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_20_095652) do
+ActiveRecord::Schema.define(version: 2019_08_21_102327) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 2019_08_20_095652) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "photo"
+    t.boolean "availability"
     t.index ["user_id"], name: "index_superpowers_on_user_id"
   end
 
