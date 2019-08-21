b3d7f7ad522129e5d8118fb216271cd4dd937bc0
Lestat McCree <lestatmccree@hotmail.com>
2019-08-21 12:57:00 +0200

    Implemented create new review - this meant I had to change the DB schema as we spoke about (added bookings_id and user_id column to review table and also removed review_id column from bookings table. Bookings has_one review + review belongs to booking

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,10 +1,27 @@
 class ReviewsController < ApplicationController
   def new
+    @booking = Booking.find(params[:booking_id])
+    @review = Review.new
   end
 
   def create
+    @review = Review.new(review_params)
+    @booking = Booking.find(params[:booking_id])
+    @review.user_id = current_user.id
+    @review.booking_id = @booking.id
+    if @review.save
+      redirect_to dashboard_path
+    else
+      render :new
+    end
   end
 
   def show
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:rating, :description)
   end
 end
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,6 +1,6 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :superpower
-  has_one :review
+  has_many :reviews
   validates :date, presence: true
 end
